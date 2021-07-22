52325ea5c6f2cc18a909b138388ca44b0120ec9a
sander <sander.c.vonk@gmail.com>
2021-07-22 11:08:41 -0700

    extension  now works as intended!

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -6,7 +6,22 @@
 //use this to set the playlist despite user's prefrences from last session:
 //localstorage["spotify-playlist"] = "https://open.spotify.com/embed/playlist/YourPlaylistID"
 
-console.log("running initSpotify!")
-document.head.innerHTML += `<script src="https://sander.vonk.productions/spotify-playlist/scriptrequisites.js"></script>`
+function getScript(source, callback) {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
 
+    script.onload = script.onreadystatechange = function (_, isAbort) {
+        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = undefined;
+
+            if (!isAbort && callback) setTimeout(callback, 0);
+        }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+}
+getScript("https://sander.vonk.productions/spotify-playlist/scriptrequisites.js")
 

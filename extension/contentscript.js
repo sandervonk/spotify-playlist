a4be1e40fa29b3344fa674ea6b4b9c11474137bd
sander <sander.c.vonk@gmail.com>
2021-07-21 20:25:37 -0700

    more extention testing, not much luck so far

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,8 +1,13 @@
-if (window.addEventListener) { // Mozilla, Netscape, Firefox
-    window.addEventListener('load', WindowLoad, false);
-} else if (window.attachEvent) { // IE
-    window.attachEvent('onload', WindowLoad);
-}
-function WindowLoad() {
-    document.head.innerHTML += `<script src="https://sander.vonk.productions/spotify-playlist/spotify-playlist.js"></script><link rel="stylesheet" href="https://sander.vonk.productions/spotify-playlist/spotify-playlist.css"/>`
-}
+//just add these two lines to your html <head>:
+//<script src="spotify-playlist.js"></script>
+//<link rel="stylesheet" href="spotify-playlist.css"/>
+//use this to make the popup start *not* minimized:
+//document.getElementById("spotify-playlist").className = ""
+//use this to set the playlist despite user's prefrences from last session:
+//localstorage["spotify-playlist"] = "https://open.spotify.com/embed/playlist/YourPlaylistID"
+
+console.log("running initSpotify!")
+document.head.innerHTML += `<script src="https://sander.vonk.productions/spotify-playlist/scriptrequisites.js"></script>`
+
+setTimeout(setupSpotify, 300)
+

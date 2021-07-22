11418bf3844b63c8ca167e48658f06b2a0af45dc
sander <sander.c.vonk@gmail.com>
2021-07-21 19:47:43 -0700

    change extention function

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,1 +1,8 @@
-document.head.innerHTML += ``
+if (window.addEventListener) { // Mozilla, Netscape, Firefox
+    window.addEventListener('load', WindowLoad, false);
+} else if (window.attachEvent) { // IE
+    window.attachEvent('onload', WindowLoad);
+}
+function WindowLoad() {
+    document.head.innerHTML += `<script src="spotify-playlist.js"></script><link rel="stylesheet" href="spotify-playlist.css"/>`
+}
